d432f95fab23bf44bb45e87728dab22e947003c7
Riley <marsharm@miamioh.edu>
2021-06-11 10:59:29 -0500

    n

--- db/seed.js
+++ db/seed.js
@@ -133,6 +133,30 @@ async function createInitialPosts() {
   }
 }
 
+async function createInitialTags() {
+  try {
+    console.log("Starting to create tags...");
+
+    const [happy, sad, inspo, catman] = await createTags([
+      '#happy', 
+      '#worst-day-ever', 
+      '#youcandoanything',
+      '#catmandoeverything'
+    ]);
+
+    const [postOne, postTwo, postThree] = await getAllPosts();
+
+    await addTagsToPost(postOne.id, [happy, inspo]);
+    await addTagsToPost(postTwo.id, [sad, inspo]);
+    await addTagsToPost(postThree.id, [happy, catman, inspo]);
+
+    console.log("Finished creating tags!");
+  } catch (error) {
+    console.log("Error creating tags!");
+    throw error;
+  }
+}
+
 async function rebuildDB() {
   try {
     client.connect();
@@ -141,6 +165,7 @@ async function rebuildDB() {
     await createTables();
     await createInitialUsers();
     await createInitialPosts();
+    await createInitialTags(); 
   } catch (error) {
     console.log("Error during rebuildDB")
     throw error;
--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const PORT = 3000;
+const PORT = 3001;
 const express = require('express');
 const server = express();
-
-const bodyParser = require('body-parser');
-server.use(bodyParser.json());
-
-const morgan = require('morgan');
-server.use(morgan('dev'));
-
-const { client } = require('./db');
-client.connect();
 
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
