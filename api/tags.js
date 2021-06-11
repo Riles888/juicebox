21b1f1f3331033b2aa7546df53c19bf219387d75
Riley <marsharm@miamioh.edu>
2021-06-11 14:25:40 -0500

    almost

--- api/tags.js
+++ api/tags.js
@@ -1,2 +1,33 @@
 const express = require('express');
 const postsRouter = express.Router();
+
+const { 
+  getAllTags,
+  getPostsByTagName
+} = require('../db');
+
+tagsRouter.get('/:tagName/posts', async (req, res) => {
+    const { tagName } = req.params;
+  
+    try {
+      const allPosts = await getPostsByTagName(tagName);
+  
+      const posts = allPosts.filter(post => {
+        if (post.active) {
+          return true;
+        }
+  
+        if (req.user && req.user.id === post.author.id) {
+          return true;
+        }
+  
+        return false;
+      })
+  
+      res.send({ posts });
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
+
+module.exports = tagsRouter;
