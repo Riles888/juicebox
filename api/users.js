c072d39252598dd2733b56d2d335ee0f8d7e803e
Riley <marsharm@miamioh.edu>
2021-06-11 13:51:04 -0500

    day 2

--- api/users.js
+++ api/users.js
@@ -6,13 +6,94 @@ usersRouter.use((req, res, next) => {
 
   next(); // THIS IS DIFFERENT
 });
-const { getAllUsers } = require('../db');
-usersRouter.get('/', async (req, res) => {
-  const users = await getAllUsers();
+const { 
+  createUser,
+  getAllUsers,
+  getUserByUsername,
+} = require('../db');
 
-  res.send({
-    users
-  });
+const jwt = require('jsonwebtoken');
+
+usersRouter.get('/', async (req, res) => {
+  try {
+    const users = await getAllUsers();
+  
+    res.send({
+      users
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+//Logging in users
+
+usersRouter.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password"
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (user && user.password == password) {
+      // create token & return to user
+      res.send({ message: "you're logged in!" });
+      
+    } else {
+      next({ 
+        name: 'IncorrectCredentialsError', 
+        message: 'Username or password is incorrect'
+      });
+    }
+  } catch(error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+//Registering Users
+
+usersRouter.post('/register', async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: 'UserExistsError',
+        message: 'A user by that username already exists'
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign({ 
+      id: user.id, 
+      username
+    }, process.env.JWT_SECRET, {
+      expiresIn: '1w'
+    });
+
+    res.send({ 
+      message: "thank you for signing up",
+      token 
+    });
+  } catch ({ name, message }) {
+    next({ name, message })
+  } 
 });
 
 module.exports = usersRouter;
